bdce4140c3eb9c339d27e9f6b4c8afba646a6b08
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-08-09 17:01:17 -0300

    Update gemini model name in defaults.go to 'gemini-2.5-flash'

--- config/defaults.go
+++ config/defaults.go
@@ -4,6 +4,6 @@ import "github.com/spf13/viper"
 
 func setDefaults() {
 	viper.SetDefault("anthropic.model_name", "claude-3-5-haiku-latest")
-	viper.SetDefault("gemini.model_name", "gemini-2.5-flash-preview-04-17")
+	viper.SetDefault("gemini.model_name", "gemini-2.5-flash")
 	viper.SetDefault("openai.model_name", "gpt-4o-mini")
 }
